4af44a110a94ee362a00711faf8ed5663cabd281
hana-ame <luminovoez@outlook.com>
2022-12-25 07:53:31 +0800

    move file

--- Portal.go
+++ Portal.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net"
+	"strings"
 	"time"
 
 	"github.com/hana-ame/udptun/utils"
@@ -11,70 +11,173 @@ import (
 
 type Portal struct {
 	*net.UDPConn
-	stunServer string
-	localAddr  string
+	// stun
+	// stunServer string
+	localAddr string
 
-	router utils.LockedMap // map[addr.String()]func([]byte)
+	// portal -> src
+	//
+	// map [addr.String()] func(PortalBuf)
+	// where addr is remote portal's address
+	// func(PortalBuf) is provided by udpmux.
+	router *utils.LockedMap
 
-	serverDst *net.UDPAddr
-	connMap   utils.LockedMap
+	// portal -> dst
+	// if dst is not nil, it will be used to send udp packets to dst
+	dst *net.UDPAddr
+	// map [addr.String() + tag] *fakeUDPConn
+	// send data for Conn here
+	connMap *utils.LockedMap
 }
 
-func (p *Portal) getLocalAddr(isIPv4 bool) string {
-	if isIPv4 {
-		if p.localAddr == "" && p.stunServer != "" {
-			go func() {
-				for {
-					utils.StunRequest(p.stunServer, p.UDPConn)
-					time.Sleep(5 * time.Second)
-				}
-			}()
+// "" means not accept remote
+func NewPortal(dst string) *Portal {
+	// addr, err := net.ResolveUDPAddr("udp", "0.0.0.0:4444") // !!!!debug
+	// c, err := net.ListenUDP("udp", addr)                   // !!!!debug
+	c, err := net.ListenUDP("udp", nil)
+	for err != nil {
+		log.Println("what?", err)
+		time.Sleep(2 * time.Second)
+		c, err = net.ListenUDP("udp", nil)
+	}
+
+	var dstAddr *net.UDPAddr = nil
+	if dst != "" {
+		dstAddr, err = net.ResolveUDPAddr("udp", dst)
+		if err != nil {
+			log.Fatal(err)
+			return nil
 		}
-		return p.localAddr
-	} else {
-		return utils.GetOutboundIPv6(p.UDPConn)
+	}
+	p := &Portal{
+		UDPConn:   c,
+		localAddr: "",
+		router:    utils.NewLockedMap(),
+		dst:       dstAddr,
+		connMap:   utils.NewLockedMap(),
+	}
+
+	go p.Run()
+
+	return p
+}
+
+func (p *Portal) Ping(arr []string) {
+	for _, v := range arr {
+		addr, err := net.ResolveUDPAddr("udp", v)
+		if err != nil {
+			log.Println(v, " can not resolve ", err)
+			continue
+		}
+		p.UDPConn.WriteToUDP([]byte{}, addr)
 	}
 }
 
-func (p *Portal) run() {
-	p.stunServer = "142.251.2.127:19302"
+// stun.
+// empty stunServer for IPv6
+// for IPv4, first stunServer is "stun.l.google.com:19302".
+// and set stunServer to "udp4" for latest result
+// for IPv6, set the stunServer to "udp" or "udp6"
+func (p *Portal) GetLocalAddr(stunServer string) string {
+	if p.localAddr == "" && !strings.HasPrefix(stunServer, "udp") {
+		go func() {
+			for {
+				utils.StunRequest(stunServer, p.UDPConn)
+				time.Sleep(30 * time.Second)
+			}
+		}()
+		time.Sleep(5 * time.Second)
+	} else if stunServer != "udp4" {
+		t := utils.GetOutboundIPv6(p.UDPConn)
+		if t != "" {
+			p.localAddr = t
+		}
+	}
+	return p.localAddr
+}
 
+// go
+func (p *Portal) Run() {
+	// portal -X-> stun
+	stunServer := "142.251.2.127:19302" // google
+	p.GetLocalAddr(stunServer)
+
+	// src --> portal -X-> portal --> dst
+	// dst --> portal -X-> portal --> src
 	for {
-		buf := make([]byte, 1500)
-		fmt.Println("reading")
-		n, addr, err := p.ReadFrom(buf)
+		buf := make(PortalBuf, 1500)
+		// fmt.Println("reading")
+		n, addr, err := p.ReadFromUDP(buf)
+		if n == 0 {
+			continue
+		}
+		// when reading error
 		if err != nil {
 			log.Println(err)
 			continue
 		}
-
-		if addr.String() == p.stunServer {
-			p.localAddr, err = utils.StunResolve(buf[:n])
+		// addrString is the address of remote Portal
+		addrString := addr.String()
+		if addrString == stunServer {
+			// stun -X-> portal
+			p.localAddr, err = utils.StunResolve(buf.Raw(n))
 			if err != nil {
 				log.Println("error when recv from stunServer", err)
-				continue
 			}
-		} else if value, ok := p.router.Get(addr.String()); ok {
-			if handler, ok := value.(func([]byte)); ok {
-				handler(buf[:n])
+		} else if v, ok := p.router.Get(addrString); ok {
+			// dst --> portal -X-> portal --> src
+			if handler, ok := v.(func(PortalBuf)); ok {
+				// handler is UDPMux.ReadFromPortal
+				handler(buf.Raw(n))
 			} else {
-				log.Println("invalid router")
-				continue
+				log.Println("invalid router") // never
 			}
-		} else {
-			log.Println("not supposed to be seen@protal run for loop")
+		} else if p.dst != nil {
+			// src --> portal -X-> portal -X-> dst
+			tag := string(buf.Tag())
+			// fmt.Println(addrString + tag)
+			if value, ok := p.connMap.Get(addrString + tag); ok {
+				if fc, ok := value.(*fakeUDPConn); ok {
+					fc.WriteToSrc(buf.Raw(n).Data(0))
+				} else {
+					log.Println(fc, ok, value)  // never
+					log.Println("invalid conn") // never
+				}
+			} else {
+				// didn't get conn
+				// create a new conn
+				c, err := net.ListenUDP("udp", nil)
+				if err != nil {
+					log.Println("DailUDP failed") // never
+					continue
+				}
+				fc := NewFakeUDPConn(
+					p.dst, c,
+					addr, p.UDPConn,
+					addrString+tag, 90, func() {
+						c.Close()
+						p.connMap.Remove(addrString + tag)
+					})
+				// dst -X-> portal --> portal --> src
+				go handleUDPConn(fc, p, tag)
+				p.connMap.Put(addrString+tag, fc)
+				fc.WriteToSrc(buf.Raw(n).Data(0))
+			}
 		}
-	}
+	} //for
 }
 
-var p *Portal
-
-func renewAddr(p *Portal, isIPv4 bool) {
-	for {
-		r := p.getLocalAddr(isIPv4)
-		if r != "" {
-			//TODO: Renew
+// dst -X-> portal --> portal --> src
+func handleUDPConn(fc *fakeUDPConn, p *Portal, tag any) {
+	defer fc.srcConn.Close()
+	buf := make(PortalBuf, 1500)
+	buf.AddTag(tag)
+	for !fc.closed {
+		n, err := fc.srcConn.Read(buf.Data(0))
+		if err != nil {
+			log.Println(err)
+			continue
 		}
-		time.Sleep(5 * time.Second)
+		fc.WriteToDst(buf.DataAndTag(n))
 	}
 }

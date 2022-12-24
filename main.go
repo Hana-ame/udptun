dedac753c1fcf0b00607147ce5d6a39ef8edc2dd
hana-ame <luminovoez@outlook.com>
2022-12-24 10:06:24 +0800

    why golps stop me from editing.

--- main.go
+++ main.go
@@ -1,19 +1,5 @@
 package main
 
-// "fmt"
-
 func main() {
-
-	// addr, _ := net.ResolveUDPAddr("udp", "127.0.0.1:1900")
-	// lc, _ := net.ListenUDP("udp", addr)
-	// NewU
-
-	// initial the Portal
-	// lc, _ := net.ListenUDP("udp", nil)
-	// p = &Portal{
-	// 	UDPConn: lc,
-	// }
-	// go p.run()
-	// go renewAddr(p, true)
 
 }
